43cc94119074d3b3e92b8b593623becc371d0031
ludgoole <ludgoole@163.com>
2018-03-24 17:54:48 +0800

    add vue && redux

--- 1.model/model.js
+++ 1.model/model.js
@@ -89,3 +89,60 @@ react
 //string: charAt indexOf substring split
 //data: this.state  param 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+vue
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//            index.html  =>main.js  =>router  =>App  =>view  =>component  =>store                 //
+//index.html: 最终渲染 输出结果
+//main.js: 最终单页 挂载节点 
+//router: 最终目录 单页走向
+//App: 单页盒子 容纳单页
+//view: 合成单页 组件集合 
+//component: 单体组件 
+//store: 最终数据
+
+
+//                                            数据流                                                //
+//data: 自给自足
+//props: 啃老族
+//store: 宝藏  
+
+//                                            store                                                //
+//state：数据库 
+//mutations: 增删改查
+//actions: 执行 || commit
+
+//                                            数据传递                                              //
+//store 元数据
+//state => state.activities.events 赋值
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+redux
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//            index.html  =>index.js  =>routerMap  =>modules  =>container  =>component  =>store                 //
+//index.html: 最终渲染 输出结果
+//index.js: 最终单页 挂载节点 
+//routerMap: 最终目录 单页走向
+//modules: 合成单页 组件集合 
+//component: 单体组件 
+//store: 最终数据
+
+
+//                                            数据流                                                //
+//state: 自给自足
+//props: 啃老族
+//store: 宝藏  
+
+//                                            store                                                //
+//reducer：数据库 增删改查 
+//store: 获取数据库
+//actions: 命令 
+//dispatch: 执行
+//connect: 连接
+
+
+//                                            数据传递                                              //
+//<Provider store={store}> 元数据
+//@connect(state => ({ counter: state.counter }),ActionCreators) 连接
+//const { counter, increment, decrement, incrementIfOdd, incrementAsync } = this.props 赋值
+/////////////////////////////////////////////////////////////////////////////////////////////////////
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # secret_of_programme
+1.class
+2.vue
+3.react
